f5d222ca18d826fddcafe36df2ce232cd34a8a08
ecppm <pyaephyomyat052@gmail.com>
2022-06-08 14:09:11 +0630

    added code in html file

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 
+posts = [
+    {
+        'author' : 'Alex',
+        'date_posted' : '8/6/22',
+        'title' : 'Blog post 1',
+        'content' : 'First post content'
+    },
+    {
+        'author' : 'Bob',
+        'date_posted' : '9/6/22',
+        'title' : 'Blog post 2',
+        'content' : 'Second post content'
+    }
+]
+
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
